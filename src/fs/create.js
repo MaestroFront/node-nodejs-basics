522da7981a2bbcab2bff616961354805fc70f941
Maestro <akievaleksandr@mail.ru>
2023-06-19 19:54:55 +0500

    feat: update create file func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs, { exists } from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const text = "I am fresh and young";
+  const path = "src/fs/files/focus.txt";
+  exists(path, (e) => {
+    e
+      ? console.log("File already exist!")
+      : fs.appendFile(path, text, () => {
+          console.log("File was created!");
+        });
+  });
 };
 
 await create();
