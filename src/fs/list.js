fb76ffa8267d9cf896e54b3457366a3630ef66a8
Maestro <akievaleksandr@mail.ru>
2023-06-20 11:17:00 +0500

    feat: update list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  fs.readdir("src/fs/files", (err, files) => {
+    if (err) {
+      console.log("There is not a folder here!");
+    } else {
+      files.forEach((item, i) => console.log(`File #${i + 1}: `, item));
+    }
+  });
 };
 
 await list();
