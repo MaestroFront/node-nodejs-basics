4d65796672e3410ea297169edb31f575ec305ab2
Maestro <akievaleksandr@mail.ru>
2023-06-20 20:52:33 +0500

    fix: update read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -2,7 +2,11 @@ import fs from "fs";
 
 const read = async () => {
   fs.readFile("src/fs/files/fileToRead.txt", "utf8", (err, data) => {
-    err ? console.log("There is not a file here!") : console.log(data);
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(data);
+    }
   });
 };
 
