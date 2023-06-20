e337ad2ee39328f3769c134c8db2bba231038944
Maestro <akievaleksandr@mail.ru>
2023-06-20 20:53:17 +0500

    fix: update list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -3,7 +3,7 @@ import fs from "fs";
 const list = async () => {
   fs.readdir("src/fs/files", (err, files) => {
     if (err) {
-      console.log("There is not a folder here!");
+        console.log(data);
     } else {
       files.forEach((item, i) => console.log(`File #${i + 1}: `, item));
     }
