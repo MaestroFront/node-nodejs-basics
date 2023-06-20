e7a8f5ed3f677a09e0c414452322f3357113e3ac
Maestro <akievaleksandr@mail.ru>
2023-06-20 10:35:30 +0500

    feat: add read file func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,9 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile("src/fs/files/fileToRead.txt", "utf8", (err, data) => {
+    err ? console.log("There is not a file here!") : console.log(data);
+  });
 };
 
 await read();
