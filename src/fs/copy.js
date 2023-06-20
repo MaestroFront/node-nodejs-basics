7d6a48da874dd8a4f5e3dafc2336b607720be563
Maestro <akievaleksandr@mail.ru>
2023-06-20 21:58:21 +0500

    feat: update copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+import fs from "fs";
+
+const throwError = (err) => {
+  const errorMessage = "FS operation failed";
+  err.message = errorMessage;
+  throw err;
+};
+
+const copyFile = (arr, pathFiles, pathFilesCopy) => {
+  arr.forEach((file) => {
+    fs.copyFile(`${pathFiles}/${file}`, `${pathFilesCopy}/${file}`, () => {
+      console.log(`File ${file} was copied!`);
+    });
+  });
+};
+
 const copy = async () => {
-    // Write your code here 
+  const pathFiles = "src/fs/files";
+  const pathFilesCopy = "src/fs/files_copy";
+
+  fs.readdir(pathFiles, async (err) => {
+    if (err) {
+      throwError(err);
+    } else {
+      fs.mkdir(pathFilesCopy, (err) => {
+        if (err) {
+          throwError(err);
+        } else {
+          console.log("Folder was created!");
+        }
+      });
+    }
+  });
+  fs.readdir(pathFiles, async (err, filesFrom) => {
+    if (err) {
+      throwError(err);
+    } else {
+      copyFile(filesFrom, pathFiles, pathFilesCopy);
+    }
+  });
 };
 
 await copy();
