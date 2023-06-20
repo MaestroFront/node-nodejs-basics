9129213c7e1e61f0bfc88c80c1305dfbf652af05
Maestro <akievaleksandr@mail.ru>
2023-06-20 10:41:06 +0500

    feat: add rename file func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  fs.rename(
+    "src/fs/files/wrongFilename.txt",
+    "src/fs/files/properFilename.md",
+    (err) => {
+      if (err) {
+        console.log("There is not a file here!");
+      } else {
+        console.log("File was rename!");
+      }
+    }
+  );
 };
 
 await rename();
