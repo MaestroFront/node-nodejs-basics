86dc18cef5ca3d1d235e39c03f2c0ad00992d747
Maestro <akievaleksandr@mail.ru>
2023-06-21 21:19:28 +0500

    feat: update stream/write func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream(
+    `${__dirname}/files/fileToWrite.txt`
+  );
+  process.stdin.pipe(writeStream);
 };
 
 await write();
