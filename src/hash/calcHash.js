5e4d5ccfdfd9ccf64b7c11367af962f272817e30
Maestro <akievaleksandr@mail.ru>
2023-06-21 19:39:52 +0500

    feat: update hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile(`${__dirname}/files/fileToCalculateHashFor.txt`, (err, data) => {
+    if (err) {
+      throw new Error("Error! Yeah!");
+    } else {
+      console.log(crypto.createHash("sha256").update(data).digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
