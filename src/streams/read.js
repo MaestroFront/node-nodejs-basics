406fd6e6fbe8a5976975278d4c50cb966b7f28e9
Maestro <akievaleksandr@mail.ru>
2023-06-21 21:02:03 +0500

    fix: update stream/read func

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,12 +2,13 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import fs, { createReadStream } from "fs";
-import stream from "node:stream";
+import fs from "fs";
 
 const read = async () => {
   const readStream = fs.createReadStream(`${__dirname}/files/fileToRead.txt`);
-  readStream.on("data", (chunk) => process.stdout.write(chunk.toString() + '\n'));
+  readStream.on("data", (chunk) =>
+    process.stdout.write(chunk.toString() + "\n")
+  );
 };
 
 await read();
