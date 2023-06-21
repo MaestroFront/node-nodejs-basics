572a723ebd8402b3fe220ef9553428988f06028d
Maestro <akievaleksandr@mail.ru>
2023-06-21 11:15:30 +0500

    feat: update parseArgs func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  let result = [];
+  for (let i = 0; i < process.argv.length; i += 2) {
+    result.push(`${process.argv[i].slice(2)} is ${process.argv[i + 1]}`);
+  }
+  result.shift();
+  console.log("RESULT: ", result.join(", "));
 };
 
 parseArgs();
