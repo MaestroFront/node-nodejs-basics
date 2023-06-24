d1e6700e3495091199f97daa534b1c55b959eb48
Maestro <akievaleksandr@mail.ru>
2023-06-24 21:02:11 +0500

    feat: update compress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import fs, { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const deleteFile = async () => {
+  fs.unlink(`${__dirname}/files/fileToCompress.txt`, () => {
+    process.stdout.write("fileToCompress.txt was deleted!\n");
+  });
+};
+const showMessage = async () =>
+  process.stdout.write("Compression process done: archive.gz!\n");
+
 const compress = async () => {
-    // Write your code here 
+  const handleStream = createReadStream(
+    `${__dirname}/files/fileToCompress.txt`
+  );
+  handleStream
+    .pipe(createGzip())
+    .pipe(createWriteStream(`${__dirname}/files/archive.gz`))
+    .on("finish", () => {
+      showMessage();
+      deleteFile();
+    });
 };
 
 await compress();
